41108f011aa21c4a8620fe80628bb9062cac81e8
Anderson Job Loeffler <aloeffler.j@gmail.com>
2021-11-20 18:53:23 -0300

    mudando links de volta para localhost

--- server/src/Views/_sidebar.php
+++ server/src/Views/_sidebar.php
@@ -1,2 +1,2 @@
-<li><a href="http://3.143.210.42/">Home</a></li>
-<li><a href="http://3.143.210.42/git">Github Projeto</a></li>
+<li><a href="http://localhost">Home</a></li>
+<li><a href="http://localhost/git">Github Projeto</a></li>
